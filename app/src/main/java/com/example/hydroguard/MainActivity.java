1a8c12568d6e3e930e7ccc46896c3d37a2b49be2
Alex <winaralex417@gmail.com>
2023-03-27 09:09:47 +0700

    coba

--- app/src/main/java/com/example/hydroguard/MainActivity.java
+++ app/src/main/java/com/example/hydroguard/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hydroguard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -13,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Button coba = (Button) findViewById(R.id.button);
+        coba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,NavBar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
