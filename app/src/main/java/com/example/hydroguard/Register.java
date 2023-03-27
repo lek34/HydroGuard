ab825deeb75e1c4766ba024d0f3e13bae2f70d57
Alex <winaralex417@gmail.com>
2023-03-27 09:41:19 +0700

    coba

--- app/src/main/java/com/example/hydroguard/Register.java
+++ app/src/main/java/com/example/hydroguard/Register.java
@@ -2,7 +2,10 @@ package com.example.hydroguard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button register = (Button) findViewById(R.id.btnRegister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this,NavBar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
