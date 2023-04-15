ee901523b5f18a11c90d3e6e292aecd48d6dcc82
DerickCc <derickchainatra@gmail.com>
2023-04-15 14:47:20 +0700

    profile, shadow

--- app/src/main/java/com/example/hydroguard/model/Todolist.java
+++ app/src/main/java/com/example/hydroguard/model/Todolist.java
@@ -9,11 +9,59 @@ import io.realm.annotations.Required;
 
 public class Todolist extends RealmObject{
 
+    @PrimaryKey
+    @Size(max = 99999999999L)
+    private Integer idtdl;
 
+    @Required
+    private String judul;
 
+    @Required
+    @Size(max = 200)
+    private String pembuat;
+
+    private String deskripsi;
 
 
     public Todolist() {
     }
 
+    public Todolist(Integer idtdl, String judul, String pembuat, String deskripsi) {
+        this.idtdl = idtdl;
+        this.judul = judul;
+        this.pembuat = pembuat;
+        this.deskripsi = deskripsi;
+    }
+
+    public Integer getIdtdl() {
+        return idtdl;
+    }
+
+    public void setIdtdl(Integer idtdl) {
+        this.idtdl = idtdl;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getPembuat() {
+        return pembuat;
+    }
+
+    public void setPembuat(String pembuat) {
+        this.pembuat = pembuat;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
 }
